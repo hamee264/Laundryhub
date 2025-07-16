6f70f944c7f5a67ab46954627d3922caddb92a3d
hamee264 <hameedrauf978@gmail.com>
2025-07-16 17:28:47 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/', // This tells Vite to assume it's hosted at the root of your domain
   plugins: [vue()],
 })
