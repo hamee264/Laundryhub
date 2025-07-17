41d583fe32d980d608303f003388303d99cfc0a3
hamee264 <hameedrauf978@gmail.com>
2025-07-17 16:38:58 +0100

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/', // This tells Vite to assume it's hosted at the root of your domain
   plugins: [vue()],
+  base: '/Laundryhub/' // Important for GitHub Pages deployment
 })
